64506ae5756f2b3f2fa28e91f955e09f34137d25
Parabeac-Nest <admin@parabeac.com>
2023-03-25 12:56:19 +0000

    Auto-generated commit by Parabeac Cloud

---
+++ android/app/src/main/kotlin/com/example/talento/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.talento
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/theme/talento_colors.g.dart
@@ -0,0 +1,24 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class TalentoColors {
+
+static const Color tikTokDark = Color(0xff161722);
+
+static const Color colorWhite = Color(0xffffffff);
+
+static const Color whitePrimary = Color(0xffffffff);
+
+static const Color buttonPrimary = Color(0xff3e3fd8);
+
+static const Color textPrimary = Color(0xff2e323b);
+
+static const Color darkPrimary = Color(0xff61646b);
+
+static const Color tikTokButton = Color(0xffea4359);
+}
